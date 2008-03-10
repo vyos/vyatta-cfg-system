e7b2082e077f2ac4625f8a7e2a45749b3ce56140
Stephen Hemminger <stephen.hemminger@vyatta.com>
2008-03-10 11:58:01 -0700

    Update to GPLv2
    Replace references to VPL 1.0 with GPLv2

--- scripts/keepalived/vyatta-keepalived.pl
+++ scripts/keepalived/vyatta-keepalived.pl
@@ -3,18 +3,15 @@
 # Module: vyatta-keepalived.pl
 # 
 # **** License ****
-# Version: VPL 1.0
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
 # 
-# The contents of this file are subject to the Vyatta Public License
-# Version 1.0 ("License"); you may not use this file except in
-# compliance with the License. You may obtain a copy of the License at
-# http://www.vyatta.com/vpl
-# 
-# Software distributed under the License is distributed on an "AS IS"
-# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-# the License for the specific language governing rights and limitations
-# under the License.
-# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
 # This code was originally developed by Vyatta, Inc.
 # Portions created by Vyatta are Copyright (C) 2007 Vyatta, Inc.
 # All Rights Reserved.
