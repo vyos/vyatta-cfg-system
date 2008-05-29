767281ed60c3dfe74dc888a0eff305a1d0e21713
Stephen Hemminger <stephen.hemminger@vyatta.com>
2008-05-29 09:31:06 -0700

    Add GPL license
    Put GPL license on this script

--- scripts/system/vyatta_update_login.pl
+++ scripts/system/vyatta_update_login.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/perl
 
+# **** License ****
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# This code was originally developed by Vyatta, Inc.
+# Portions created by Vyatta are Copyright (C) 2007 Vyatta, Inc.
+# All Rights Reserved.
+#
+# **** End License ****
+
 use strict;
 use lib "/opt/vyatta/share/perl5";
 use VyattaConfig;
