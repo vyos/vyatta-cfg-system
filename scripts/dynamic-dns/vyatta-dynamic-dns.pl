vyos/vyatta-cfg-system

commit 5d27f2ef302dc600ea737e9af4151041aa11a2e0

Cleanup dynamic DNS script
Remove reference to unused Switch (Bug 6888).
Use mkdir perl function rather than calling system

scripts/dynamic-dns/vyatta-dynamic-dns.pl
@@ -30,7 +30,6 @@ use Getopt::Long;
 
 use strict;
 use warnings;
-use Switch;
 
 my $ddclient_run_dir = '/var/run/ddclient';
 my $ddclient_cache_dir = '/var/cache/ddclient';
@@ -55,13 +54,8 @@ if (defined $update_dynamicdns) {
    dynamicdns_restart();
 }
 
-if (defined $op_mode_update_dynamicdns) {
-    dynamicdns_restart();
-}
-
-if (defined $stop_dynamicdns) {
-    dynamicdns_stop();
-}
+dynamicdns_restart() if (defined $op_mode_update_dynamicdns);
+dynamicdns_stop()    if (defined $stop_dynamicdns);
 
 exit 0;
 
@@ -75,13 +69,10 @@ sub dynamicdns_restart {
 }
 
 sub dynamicdns_start {
-
-    if(! -d $ddclient_run_dir ){
-            system ("mkdir $ddclient_run_dir\;");
-    }
-    if(! -d $ddclient_cache_dir ){
-            system ("mkdir $ddclient_cache_dir\;");
-    }
+    mkdir $ddclient_run_dir
+	unless ( -d $ddclient_run_dir );
+    mkdir $ddclient_cache_dir
+	unless ( -d $ddclient_cache_dir );
 
     system("/usr/sbin/ddclient -file $ddclient_config_dir/ddclient_$interface.conf >&/dev/null");
 
@@ -141,13 +132,12 @@ sub dynamicdns_get_values {
 sub dynamicdns_write_file {
     my ($config) = @_;
 
-    if(! -d $ddclient_config_dir ){
-            system ("mkdir $ddclient_config_dir\;");
-    }
-    open(my $fh, '>', "$ddclient_config_dir/ddclient_$interface.conf") || die "Couldn't open \"$ddclient_config_dir/ddclient_$interface.conf\" - $!";
+    mkdir $ddclient_config_dir
+	unless (-d $ddclient_config_dir );
+
+    open(my $fh, '>', "$ddclient_config_dir/ddclient_$interface.conf")
+	|| die "Couldn't open \"$ddclient_config_dir/ddclient_$interface.conf\" - $!";
     print $fh $config;
     close $fh;
 }
 
-
-# end of file

commit 4df1ff6a97924558640dacf4135b367a1d156af4

Fix dyndns failure caused by commit: 5d27f2ef302dc600ea737e9af4151041aa11a2e0

scripts/dynamic-dns/vyatta-dynamic-dns.pl
@@ -107,11 +107,9 @@ sub dynamicdns_get_values {
     my @services = $config->listNodes("service");
     foreach my $service (@services) {
        $config->setLevel("service dns dynamic interface $interface service $service");
-       switch ($service) {
-           case "dslreports" {$service="dslreports1";}
-           case "dyndns"     {$service="dyndns2";}
-           case "zoneedit"   {$service="zoneedit1";}
-       }
+       $service="dslreports1" if ($service eq "dslreports");
+       $service="dyndns2" if ($service eq "dyndns");
+       $service="zoneedit1" if ($service eq "zoneedit");
        my $login = $config->returnValue("login");
        my $password = $config->returnValue("password");
        my @hostnames = $config->returnValues("host-name");