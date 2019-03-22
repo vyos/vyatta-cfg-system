6d53089a202eac77b9a6599718807ee77ae8b49c
Ruben Herold <ruben@puettmann.net>
2019-03-22 08:11:10 +0100

    T1309: remove duplicate ip address check
    (cherry picked from commit 67b10e6ff5f33d73c128508f00198cc3c04c13fc)

--- scripts/vyatta-interfaces.pl
+++ scripts/vyatta-interfaces.pl
@@ -314,11 +314,6 @@ sub is_valid_addr_commit {
       next;
     }
 
-    my $intfs = $addrmap->{$addr};
-    if ($intfs && scalar(@{$intfs}) > 1) {
-      die "Duplicate address $addr used on interfaces: ",join(',', @${intfs}), "\n";
-    }
-
     $static_v4 = 1
       if (is_ipv4($addr));
   }
